253c4b982401dbdb6d0e23090dbe2f50a3dbb76a
Rafaela Mielli Bonacim <ra_bonacim@magazineluiza.intranet>
2020-11-20 21:05:06 -0300

    teste Produto

--- products/tests.py
+++ products/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Seller
+from .models import Seller, Product
 from rest_framework.test import APIClient
 import json
 
@@ -21,3 +21,59 @@ class SellerViewTests(TestCase):
         seller1 = data.get('results')[0]
 
         self.assertEqual(seller1.get("name"), "Jaqueloja")
+
+    def test_post(self):
+        client = APIClient()
+        response = client.post('/products/sellers/', {
+            "name": "Batman",
+        })
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEquals(Seller.objects.count(), 2)
+        self.assertEquals(Seller.objects.last().name, "Batman")
+
+
+class ProductViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        seller = Seller.objects.create(name="Jaqueloja")
+        Product.objects.create(code=123,name="batman",price=200.00,stock_quantity=40,status="A",seller=seller)
+       
+    def test_get(self):
+        client = APIClient()
+        response = client.get('/products/')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+
+        self.assertEqual(data.get('count'), 1)
+
+        product1 = data.get('results')[0]
+
+        self.assertEqual(product1.get("code"), 123)
+        self.assertEqual(product1.get("name"), "batman")
+        self.assertEqual(product1.get("price"), 200.00)
+        self.assertEqual(product1.get("stock_quantity"), 40)
+        self.assertEqual(product1.get("status"), "A")
+
+    def test_post(self):
+        client = APIClient()
+        response = client.post('/products/', {
+            "code": 123,
+            "name":"batman",
+            "price":200.00,
+            "stock_quantity":40,
+            "status":"A",
+            "seller": "http://testserver/products/sellers/1/"
+            })
+        product = Product.objects.last()
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEquals(Product.objects.count(), 2)
+        self.assertEquals(product.code, 123)
+        self.assertEquals(product.name, "batman")
+        self.assertEquals(product.price, 200.00)
+        self.assertEquals(product.stock_quantity, 40)
+        self.assertEquals(product.status, "A")
+        
